ecfe6fa2dadf0488368005817f5b19b57eeb56aa
imshubham <jangleshubham@ymail.com>
2020-07-14 18:55:59 +0530

    added button listener and its functionality

--- app/src/main/java/com/octa/netspeedmeter/MainActivity.java
+++ app/src/main/java/com/octa/netspeedmeter/MainActivity.java
@@ -3,6 +3,9 @@ package com.octa.netspeedmeter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button pushButton = (Button) findViewById(R.id.buttonPush);
+        final TextView mainText = (TextView) findViewById(R.id.textViewMain);
+
+        pushButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainText.setText("Hey Shubham!");
+            }
+        });
     }
 }
