96799bbcbce1f25f64ab7c43cde0cfa620fda902
imshubham <jangleshubham@ymail.com>
2020-07-30 20:46:55 +0530

    made app full screen

--- app/src/main/java/com/octa/netspeedmeter/MainActivity.java
+++ app/src/main/java/com/octa/netspeedmeter/MainActivity.java
@@ -3,25 +3,45 @@ package com.octa.netspeedmeter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public boolean doubleBackToExitPressedOnce;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        doubleBackToExitPressedOnce = false;
 
-        Button pushButton = (Button) findViewById(R.id.buttonPush);
-        final TextView mainText = (TextView) findViewById(R.id.textViewMain);
+        Button startButton = (Button) findViewById(R.id.buttonStart);
+        TextView textViewdownload = (TextView) findViewById(R.id.download);
+        TextView textViewUpload = (TextView) findViewById(R.id.upload);
 
-        pushButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mainText.setText("Hey Shubham!");
-            }
-        });
+
+    }
+
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+        } else {
+            doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Press Again To Exit...!", Toast.LENGTH_SHORT).show();
+            mHandler.postDelayed(new Runnable() {
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 5000);
+        }
     }
 }
